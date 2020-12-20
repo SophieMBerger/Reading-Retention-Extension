1382129607991274e100435105e7b1c4286d9c4e
SophieMBerger <berger.m.sophie@gmail.com>
2020-12-20 14:07:46 +0100

    Showing form when at bottom of page

--- background.js
+++ background.js
@@ -2,7 +2,9 @@ const iconRules = [{
     conditions: [
         new chrome.declarativeContent.PageStateMatcher({
             pageUrl: {hostEquals: 'medium.com'},
-            pageUrl: {hostEquals: 'wikipedia.org'}
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: 'en.wikipedia.org'}
         })
     ],
     actions: [new chrome.declarativeContent.ShowPageAction()]
@@ -10,3 +12,17 @@ const iconRules = [{
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules(iconRules);
 });
+
+// Receive message from contentScript when bottom of page is reached
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+      if (request.message == "bottom")
+        // window.open("popup.html", window.name, "width=500,height=500,status=no,scrollbars=no,resizable=yes, top=0, left=0");
+        chrome.tabs.update({url:"popup.html"}); 
+        // create({url:"popup.html"});
+        sendResponse({response: "true"});
+    }
+  );
--- contentScript.js
+++ contentScript.js
@@ -2,5 +2,10 @@ window.onscroll = function(ev) {
     if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
       // you're at the bottom of the page
       console.log("ContentScript is running");
+
+      // Send message form contentScript to background Script
+      chrome.runtime.sendMessage({message: "bottom"}, function(response) {
+        console.log(response.response);
+      });
     }
 };
